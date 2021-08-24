395e3f6c01276521adeb1508e1b9b16010846d49
Groot <413952020@qq.com>
2021-08-24 22:23:00 +0800

    20210824

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -4,7 +4,15 @@ import urllib
 from django.urls import path
 from . import views
 
+app_name='polls'
 urlpatterns=[
     path('', views.index, name='index'),
+
+    path('<int:question_id>/',views.detail,name='detail'),  #<int:question_id> 使用尖括号“捕获”这部分 URL，匹配question_id
+
+    path('<int:question_id>/results/', views.results, name='results'),
+
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
+
 
