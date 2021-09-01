5bcf4cf7ceadd356142f768ddfdf2eae0a26d980
Groot <413952020@qq.com>
2021-09-01 23:18:54 +0800

    20210901

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -9,13 +9,15 @@ from django.utils import timezone
 
 class Question(models.Model):
     question_text=models.CharField(max_length=200)
-    pub_date=models.DateTimeField(auto_now_add=True)
+    pub_date = models.DateTimeField("date published")  #给index页面一个js显示date published
+
+    #pub_date=models.DateTimeField(auto_now_add=True)
 
     def __str__(self):    #对象的描述
         return self.question_text
 
     def was_published_recently(self):
-        return self.pub_date
+        return self.pub_date >=timezone.now() - datetime.timedelta(days=1)
 
 
 class Choice(models.Model):
--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -6,13 +6,12 @@ from . import views
 
 app_name='polls'
 urlpatterns=[
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
 
-    path('<int:question_id>/',views.detail,name='detail'),  #<int:question_id> 使用尖括号“捕获”这部分 URL，匹配question_id
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),  #<int:question_id> 使用尖括号“捕获”这部分 URL，匹配question_id
 
-    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
 
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
-
 
